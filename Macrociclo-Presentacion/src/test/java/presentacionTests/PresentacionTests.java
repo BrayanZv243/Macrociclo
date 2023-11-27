47a9896df359c319b104b15a1d30ff4c0ac1a8ae
Brayan Zavala <brayan.zavala228311@potros.itson.edu.mx>
2023-11-27 13:29:55 -0700

    Pruebas Unitarias Validar Semanas

--- Macrociclo-Presentacion/src/test/java/presentacionTests/PresentacionTests.java
+++ Macrociclo-Presentacion/src/test/java/presentacionTests/PresentacionTests.java
@@ -1,11 +1,7 @@
 package presentacionTests;
 
-import Frames.SemanasFrame;
+import java.util.Calendar;
 import java.util.Date;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -15,38 +11,43 @@ import static org.junit.Assert.*;
  * @author Brayan Zavala.
  */
 public class PresentacionTests {
-
+    
     public PresentacionTests() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
     }
 
     /**
      * Test para calcular la semanas en un periodo válido
      */
     @Test
-    public void testCalcularSemanasTest() {
+    public void calcularSemanasTest() {
         System.out.println("Calculamos las semanas de un periodo dado correcto.");
-
-        SemanasFrame semanasFrame = new SemanasFrame();
         
-        int semanas = semanasFrame.calcularSemanas(new Date(), new Date());
-        assertEquals(true,true);
+        Date fechaInicio = new Date(2023,11, 28);
+        Date fechaFin = new Date(2023,12, 19);
+        
+        validacionesSemanas(fechaInicio, fechaFin);
+        
     }
-   
+    
+    private void validacionesSemanas(Date inicio, Date fin) {
+        if (inicio == null && fin == null) {
+            fail("La fecha de inicio es nula");
+        }
+        // Validar que la fecha de inicio sea anterior a la fecha de fin
+        if (inicio.after(fin)) {
+            fail("La fecha de inicio del plan debe ser anterior a la fecha de fin del plan.");
+        }
 
+        // Validar que el período sea de al menos 7 días
+        long diferenciaEnMillis = fin.getTime() - inicio.getTime();
+        long diferenciaEnDias = diferenciaEnMillis / (24 * 60 * 60 * 1000);
+        if (diferenciaEnDias < 7) {
+            fail("El período debe ser de al menos 7 días.");
+        }
+        
+        assertTrue(true);
+    }
+    
+    
+    
 }
